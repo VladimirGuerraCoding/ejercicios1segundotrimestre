61fd6341f1fec80554071bda3ca9cb1264535198
Vladimir <vladimir.guerra.c@gmail.com>
2025-05-01 15:13:05 -0500

    segundo cargue de ejercicios

---
+++ par_impar.py
@@ -0,0 +1,8 @@
+#Evaluar si un numero es par 0 impar
+num1 = int(input('Ingresa un numero para evaluar: '))
+if num1 % 2 == 0:
+    print('El numero ingresado es par.')
+else:
+    print('El numero ingresado es impar')
+    
+        
---
+++ perimetrocirculo.py
@@ -0,0 +1,4 @@
+pi = 3.1416
+radio = 7
+perimetro = 2 * pi * radio
+print(f'el perímetro del círculo es {perimetro}')
