d94a668adc729ccda7dc4a93957c09f63a67b69e
Vladimir <vladimir.guerra.c@gmail.com>
2025-05-03 15:06:10 -0500

    segunda carga

---
+++ doble_triple_raiz.py
@@ -0,0 +1,6 @@
+num1 = int(input("¿Cuál es el primer número?:  "))
+
+print(f'El doble del numero ingresado es: {num1 * 2}')
+print(f'El triple del numero ingresado es: {num1 * 3}')
+print(f'La raiz cuadrada del numero ingresado es: {num1 ** (1/2)}')
+
